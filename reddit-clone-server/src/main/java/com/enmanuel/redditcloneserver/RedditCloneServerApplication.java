d8dcefca23b41a45adfe1fabb989554d2051a23c
Enmanuel Mendez <katariinaop@gmail.com>
2020-06-16 14:12:06 -0400

    Servidor agregado. Listo para usar.

---
+++ reddit-clone-server/src/main/java/com/enmanuel/redditcloneserver/RedditCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.enmanuel.redditcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneServerApplication.class, args);
+	}
+
+}
---
+++ reddit-clone-server/src/test/java/com/enmanuel/redditcloneserver/RedditCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enmanuel.redditcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
